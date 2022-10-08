faf6c08f86b97a247d6191eef172f7d7ec5c61fd
sagar <skatahre@gmail.com>
2022-10-08 21:08:45 +0530

    program to find out Quotient and Remainder.

---
+++ QuotientRemainder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+  
+     public static class QuotientRemainder
+    {
+        public static void Check()
+        {
+            int divident , divisor  , remainder;
+
+            
+            Console.WriteLine("enter divident");
+
+            divident = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter divisor");
+            divisor = Convert.ToInt32(Console.ReadLine());
+
+            int quotient = Math.DivRem(divident, divisor, out remainder);
+
+
+            Console.WriteLine("\n Divident : {0}  Divisor : {1} ", divident, divisor);
+            Console.WriteLine("Quotient =   \n  "+ quotient);
+            Console.WriteLine( "remainder = " +remainder );
+        }
+    }
+
+}

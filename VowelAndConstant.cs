8c2ab0657e2f1cebeec29ca38b0151fefca54e09
sagar <skatahre@gmail.com>
2022-10-08 23:20:01 +0530

    program to check if character is vowel or constant .

---
+++ VowelAndConstant.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    public class VowelAndConstant
+    {
+        public static void Alphabet()
+        {
+            char ch;
+            Console.WriteLine("Enter any Alphabet ");
+            ch = Convert.ToChar(Console.ReadLine());
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'
+                || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                Console.WriteLine(ch + "  - is  a Vowel. ");
+            }
+            else  
+            {
+                Console.WriteLine(ch + "  - is not an vowel. ");
+            }
+
+        }
+    }
+}

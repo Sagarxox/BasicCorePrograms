0b2409a9bc6ac8e8de82c6ac1b9f38278a1d24aa
sagar <skatahre@gmail.com>
2022-10-08 20:34:50 +0530

    Prints the Nth harmonic number: 1/1 + 1/2 + ... + 1/N

---
+++ HarmonicNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+
+    public class HarmonicNumber
+    {
+        public static  void Harmonic()
+        {
+            int i, n;
+            double s = 0.0;
+
+            Console.Write("\n\n");
+            Console.Write("Calculate the harmonic series and their sum:\n");
+
+            
+            Console.Write("\n\n");
+
+            Console.Write("Input the number of terms : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            if(n == 0)
+            {
+                return ;                                        // to ensure the value of n is More then  0 
+            }
+            Console.Write("\n\n");
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("1/{0} + ", i);
+                s += 1 / (float)i;
+            }
+            Console.Write("\nSum of Series upto {0} terms : {1} \n", n, s);
+        }
+    }
+
+}

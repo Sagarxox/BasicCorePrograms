f37119d4b44ffed04176aa905087756102396897
sagar <skatahre@gmail.com>
2022-10-08 20:38:40 +0530

    Computes the prime factorization of N using brute force.

---
+++ PrimeFactor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    public class Factors
+     {
+        public static void PrimeFactor ()
+        {
+           
+                int n, i;
+                Console.WriteLine("Enter the Number:");
+                n = int.Parse(Console.ReadLine());
+                Console.WriteLine("Factors of {0}:" ,n);
+                for (i = 1; i <= n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+                Console.ReadLine();
+            
+        }
+
+    }
+}

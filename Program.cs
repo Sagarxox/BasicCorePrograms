d1b818741dfbbfe2fe3a588e45e43ce040325700
sagar <skatahre@gmail.com>
2022-10-08 20:00:53 +0530

    Basic Core Programs

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace project
+{
+    public  class A
+
+    {
+
+
+        static void Main(string[] args)
+
+        {
+
+
+
+             FlipCoin.CalculatePercentage();
+
+            // Power2.Power();
+
+            //  HarmonicNumber.Harmonic();
+
+           //  Factors.PrimeFactor();
+            
+
+
+
+        }
+        
+    }
+}
